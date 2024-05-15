3b7b3789693be97c076086169f8bbf38dde7fe10
marcelo8hdz <marcelohdz.8.11@gmail.com>
2024-05-15 16:31:16 -0600

    bash scripts

--- src/bashCommands/zoso.sh
+++ src/bashCommands/zoso.sh
@@ -1,2 +1,6 @@
 #!bin/sh 
-./CocoSourcesCPP/Coco src/ZososcriptGrammar.cpp.atg -namespace Zoso -o ./src/cpp
+./CocoSourcesCPP/Coco src/ZososcriptGrammar.cpp.atg -namespace Zoso -o ./src/cpp 
+cd src/cpp 
+g++ *.cpp -o Zoso -g -Wall
+./Zoso Test.zozo
+cd ../..
